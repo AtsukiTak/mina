32771e70114e4bfe899658e77681df48c95896e9
AtsukiTak <takatomgoo@gmail.com>
2020-09-24 16:00:44 +0900

    KeychainServiceのテストの追加

--- minaTests/minaTests.swift
+++ minaTests/minaTests.swift
@@ -10,18 +10,28 @@ import XCTest
 @testable import mina
 
 class minaTests: XCTestCase {
+    
+    var keychain: KeychainService = KeychainService(serviceName: "me.atsuki.minaTests")
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // 保存したものを削除する
+        try keychain.deleteCred()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testKeychain() throws {
+        // 最初は何も保存されていない
+        XCTAssertEqual(try keychain.readCred(), nil)
+        
+        // 適切に保存できる
+        try keychain.saveCred(cred: Credential(username: "atsuki", password: "hoge"))
+        
+        // 保存したものを取得できる
+        let saved = try keychain.readCred()!
+        XCTAssertEqual(saved.username, "atsuki")
+        XCTAssertEqual(saved.password, "hoge")
     }
 
     func testPerformanceExample() throws {
