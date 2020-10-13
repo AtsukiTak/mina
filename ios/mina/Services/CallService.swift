d618e7616ff9ad71a443c55c203cb7994a08b086
AtsukiTak <takatomgoo@gmail.com>
2020-10-13 22:55:49 +0900

    通話プロセスの開始

--- ios/mina/Services/CallService.swift
+++ ios/mina/Services/CallService.swift
@@ -50,6 +50,7 @@ final class CallService: NSObject {
                 completion()
             } else {
                 // systemが着信を許可したので通話プロセスを開始する
+                CallManager.shared.start()
             }
         }
     }
