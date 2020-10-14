ac32c4d100ee14ee4c74de94e3d736ddde0eb919
AtsukiTak <takatomgoo@gmail.com>
2020-10-14 18:33:20 +0900

    コメントの追加

--- server/src/usecase/signup.rs
+++ server/src/usecase/signup.rs
@@ -5,6 +5,24 @@ pub struct Params {
     name: String,
 }
 
+/// 超絶シンプルな登録プロセス
+/// Appは初回起動時に名前をサーバーに登録し、返されたUserId
+/// をApp内に保存する。
+/// 以降、そのUserIdを使ってAPIリクエストを行う
+///
+/// ## Production Ready Signup
+/// - Signin with Apple Id を利用する
+/// - Appは起動時にサーバーと接続し、nonceを取得
+/// - サーバーは発効したnonceとSession IDを保存しておく
+/// - Appはnonceを付与したSignin with Apple IDリクエストを
+///   Appleサーバーに送り、Identity Tokenを取得する
+/// - Appは取得したIdentity Tokenをサーバーに送る
+/// - サーバーは送られてきたIdentity Tokenを検証する
+///   - nonceは合っているか
+///   - Signatureは合っているか、etc..
+/// - Tokenをパースして得られたUserIdとSessionIdを紐づける
+/// - nonceを無効化する
+/// - SessionIdを用いてsignupを行う
 pub async fn signup<R>(Params { name }: Params, repo: &mut R) -> Result<User, Error>
 where
     R: UserRepository,
