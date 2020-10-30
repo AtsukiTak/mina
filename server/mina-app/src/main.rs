187b0cfef1b93f816f9dde88584af957cbd513fd
AtsukiTak <takatomgoo@gmail.com>
2020-10-30 17:56:12 +0900

    serverを起動する部分の実装

--- server/mina-app/src/main.rs
+++ server/mina-app/src/main.rs
@@ -1,6 +1,22 @@
-use chrono::Utc;
-use mina_app::infra::apple::push::{Authorizer, Client};
-use serde::Serialize;
+use mina_app::server::{bind, Config};
 
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    let db_url = get_env_var_or_panic("DATABASE_URL");
+
+    let port = get_env_var_u16("PORT").unwrap_or(8080);
+
+    let config = Config::new(db_url.as_str()).await;
+
+    bind(([0, 0, 0, 0], port), config).await
+}
+
+fn get_env_var_or_panic(key: &'static str) -> String {
+    std::env::var(key).unwrap_or_else(|_| panic!(format!("{} is not specified", key)))
+}
+
+fn get_env_var_u16(key: &'static str) -> Option<u16> {
+    std::env::var(key)
+        .ok()
+        .map(|s| u16::from_str_radix(s.as_str(), 10).unwrap())
+}
