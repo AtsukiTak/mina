0f639434b2e2f087c87318a34bf2df438cf7f0d5
AtsukiTak <takatomgoo@gmail.com>
2020-11-10 16:40:09 +0900

    add add_call_schedule usecase

---
+++ server/mina-usecase/src/add_call_schedule.rs
@@ -0,0 +1,35 @@
+use crate::auth::AuthenticatedUser;
+use chrono::{NaiveTime, Weekday};
+use mina_domain::{
+    relationship::{Relationship, RelationshipRepository as _},
+    RepositorySet,
+};
+use rego::Error;
+use uuid::Uuid;
+
+pub async fn add_call_schedule<R>(
+    relationship_id: Uuid,
+    weekdays: impl IntoIterator<Item = Weekday>,
+    time: NaiveTime,
+    me: &AuthenticatedUser,
+    repos: &R,
+) -> Result<Relationship, Error>
+where
+    R: RepositorySet,
+{
+    // `find_by_id` メソッドを用意するのが面倒なので
+    // `find_of_user` メソッドを再利用している
+    let mut relationship = repos
+        .relationship_repo()
+        .find_of_user(me.as_ref().id())
+        .await?
+        .into_iter()
+        .find(|rel| *rel.id().as_ref() == relationship_id)
+        .ok_or_else(|| Error::bad_input("specified relationship not found"))?;
+
+    relationship.add_call_schedule(weekdays, time);
+
+    repos.relationship_repo().update(&relationship).await?;
+
+    Ok(relationship)
+}
--- server/mina-usecase/src/lib.rs
+++ server/mina-usecase/src/lib.rs
@@ -1,8 +1,10 @@
 mod accept_partner_request;
+mod add_call_schedule;
 pub mod auth;
 mod send_partner_request;
 mod signup;
 
 pub use accept_partner_request::accept_partner_request;
+pub use add_call_schedule::add_call_schedule;
 pub use send_partner_request::send_partner_request;
 pub use signup::signup_as_anonymous;
