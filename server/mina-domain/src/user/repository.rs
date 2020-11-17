5322566e432a3f944fdc2807e5a86f34db8bb0f3
AtsukiTak <takatomgoo@gmail.com>
2020-11-17 18:57:32 +0900

    UserRepository::find_by_idsの追加

--- server/mina-domain/src/user/repository.rs
+++ server/mina-domain/src/user/repository.rs
@@ -1,9 +1,18 @@
 use super::{User, UserId};
 use rego::Error;
+use std::{collections::HashMap, slice};
 
 #[async_trait::async_trait]
-pub trait UserRepository {
-    async fn find_by_id(&self, user_id: &UserId) -> Result<User, Error>;
+pub trait UserRepository: Send + Sync {
+    async fn find_by_ids(&self, user_ids: &[UserId]) -> HashMap<UserId, Result<User, Error>>;
+
+    async fn find_by_id(&self, user_id: &UserId) -> Result<User, Error> {
+        self.find_by_ids(slice::from_ref(user_id))
+            .await
+            .get(user_id)
+            .unwrap()
+            .clone()
+    }
 
     async fn create(&self, user: &User) -> Result<(), Error>;
 
--- server/mina-infra/src/repository/user/mod.rs
+++ server/mina-infra/src/repository/user/mod.rs
@@ -3,11 +3,10 @@ pub use self::pg::SnapshotHash;
 
 use self::pg::{insert, load, update};
 use super::PgClient;
-use dataloader::{non_cached::Loader, BatchFn};
+use dataloader::{cached::Loader, BatchFn};
 use mina_domain::user::{User, UserId, UserRepository};
 use rego::Error;
 use std::{collections::HashMap, ops::DerefMut as _};
-use tokio::sync::Mutex;
 
 /// ## 責務
 /// - DBに対する操作（SQL操作）
@@ -16,7 +15,6 @@ use tokio::sync::Mutex;
 /// - 楽観ロック
 pub struct UserRepositoryImpl {
     client: PgClient,
-    cache: Mutex<HashMap<UserId, Result<(User, SnapshotHash), Error>>>,
     loader: Loader<UserId, Result<(User, SnapshotHash), Error>, UserLoader>,
 }
 
@@ -24,7 +22,6 @@ impl UserRepositoryImpl {
     pub fn new(client: PgClient) -> UserRepositoryImpl {
         UserRepositoryImpl {
             client: client.clone(),
-            cache: Mutex::new(HashMap::new()),
             loader: Loader::new(UserLoader::new(client)),
         }
     }
@@ -32,24 +29,29 @@ impl UserRepositoryImpl {
 
 #[async_trait::async_trait]
 impl UserRepository for UserRepositoryImpl {
-    async fn find_by_id(&self, user_id: &UserId) -> Result<User, Error> {
-        let cache = self.cache.lock().await;
-        if let Some(cached) = cache.get(user_id).cloned() {
-            return cached.map(|(user, _)| user);
-        }
-        drop(cache);
+    async fn find_by_ids(&self, user_ids: &[UserId]) -> HashMap<UserId, Result<User, Error>> {
+        self.loader
+            .load_many(user_ids.to_vec())
+            .await
+            .into_iter()
+            .map(|(id, val)| (id, val.map(|(user, _)| user)))
+            .collect()
+    }
 
-        Ok(self.loader.load(user_id.clone()).await?.0)
+    async fn find_by_id(&self, user_id: &UserId) -> Result<User, Error> {
+        self.loader
+            .load(user_id.clone())
+            .await
+            .map(|(user, _)| user)
     }
 
     async fn create(&self, user: &User) -> Result<(), Error> {
         // DBへの挿入
         let new_hash = insert(self.client.lock().await.deref_mut(), user).await?;
 
-        self.cache
-            .lock()
-            .await
-            .insert(user.id().clone(), Ok((user.clone(), new_hash)));
+        self.loader
+            .prime(user.id().clone(), Ok((user.clone(), new_hash)))
+            .await;
 
         Ok(())
     }
@@ -63,10 +65,9 @@ impl UserRepository for UserRepositoryImpl {
         // DBのupdate
         let new_hash = update(self.client.lock().await.deref_mut(), user, cached.1).await?;
 
-        self.cache
-            .lock()
-            .await
-            .insert(user.id().clone(), Ok((user.clone(), new_hash)));
+        self.loader
+            .prime(user.id().clone(), Ok((user.clone(), new_hash)))
+            .await;
 
         Ok(())
     }
