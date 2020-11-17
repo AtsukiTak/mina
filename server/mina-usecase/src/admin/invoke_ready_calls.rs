03375982da967be2a0e9e3d1064f986d2c920441
AtsukiTak <takatomgoo@gmail.com>
2020-11-17 19:10:47 +0900

    admin::invoke_ready_callsが関連するUserの一覧を返すように修正

--- server/mina-usecase/src/admin/invoke_ready_calls.rs
+++ server/mina-usecase/src/admin/invoke_ready_calls.rs
@@ -1,17 +1,24 @@
 use chrono::Utc;
 use mina_domain::{
     relationship::{Call, RelationshipRepository as _},
+    user::{User, UserId, UserRepository as _},
     RepositorySet,
 };
 use rego::Error;
+use std::collections::HashMap;
 
-pub async fn invoke_ready_calls<R>(repos: &R) -> Result<Vec<Call>, Error>
+pub struct Res {
+    pub calls: Vec<Call>,
+    pub users: HashMap<UserId, User>,
+}
+
+pub async fn invoke_ready_calls<R>(repos: &R) -> Result<Res, Error>
 where
     R: RepositorySet,
 {
     let now = Utc::now();
 
-    // readyなcallがあるrelationshipの一覧
+    // readyなcallがあるrelationshipの一覧を取得
     let mut relationships = repos
         .relationship_repo()
         .find_all()
@@ -25,10 +32,26 @@ where
         .map(|rel| rel.start_call_process_at(now).unwrap().clone())
         .collect::<Vec<_>>();
 
+    // 関連するUser一覧を取得
+    let mut user_ids = Vec::with_capacity(relationships.len() * 2);
+    for relationship in relationships.iter() {
+        let [u1, u2] = relationship.users();
+        user_ids.push(u1.clone());
+        user_ids.push(u2.clone());
+    }
+    let users = repos
+        .user_repo()
+        .find_by_ids(user_ids.as_slice())
+        .await
+        .into_iter()
+        .filter_map(|(id, res)| res.ok().map(|user| (id, user)))
+        .collect();
+
+    // 更新したrelationshipの保存
     repos
         .relationship_repo()
         .update_many(relationships.as_slice())
         .await?;
 
-    Ok(calls)
+    Ok(Res { calls, users })
 }
