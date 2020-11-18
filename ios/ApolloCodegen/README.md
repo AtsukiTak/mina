5952824c2aa7096b32ced7a996051d5c33c7265b
AtsukiTak <takatomgoo@gmail.com>
2020-11-18 14:22:18 +0900

    Apollo, ApolloCodegenの導入

---
+++ ios/ApolloCodegen/README.md
@@ -0,0 +1,11 @@
+# ApolloCodegen
+
+## Run
+
+```bash
+swift run
+```
+
+## Note
+
+- `Package.swift` の `dependencies` に記載しているApolloパッケージのバージョンを、メインのコードで使用しているApolloのバージョンと一致させる
---
+++ ios/mina/GraphQL/API.swift
@@ -0,0 +1,102 @@
+// @generated
+//  This file was automatically generated and should not be edited.
+
+import Apollo
+import Foundation
+
+public final class MeQuery: GraphQLQuery {
+  /// The raw GraphQL definition of this operation.
+  public let operationDefinition: String =
+    """
+    query Me {
+      me {
+        __typename
+        id
+        name
+      }
+    }
+    """
+
+  public let operationName: String = "Me"
+
+  public init() {
+  }
+
+  public struct Data: GraphQLSelectionSet {
+    public static let possibleTypes: [String] = ["Query"]
+
+    public static var selections: [GraphQLSelection] {
+      return [
+        GraphQLField("me", type: .nonNull(.object(Me.selections))),
+      ]
+    }
+
+    public private(set) var resultMap: ResultMap
+
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(me: Me) {
+      self.init(unsafeResultMap: ["__typename": "Query", "me": me.resultMap])
+    }
+
+    public var me: Me {
+      get {
+        return Me(unsafeResultMap: resultMap["me"]! as! ResultMap)
+      }
+      set {
+        resultMap.updateValue(newValue.resultMap, forKey: "me")
+      }
+    }
+
+    public struct Me: GraphQLSelectionSet {
+      public static let possibleTypes: [String] = ["Me"]
+
+      public static var selections: [GraphQLSelection] {
+        return [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("id", type: .nonNull(.scalar(String.self))),
+          GraphQLField("name", type: .scalar(String.self)),
+        ]
+      }
+
+      public private(set) var resultMap: ResultMap
+
+      public init(unsafeResultMap: ResultMap) {
+        self.resultMap = unsafeResultMap
+      }
+
+      public init(id: String, name: String? = nil) {
+        self.init(unsafeResultMap: ["__typename": "Me", "id": id, "name": name])
+      }
+
+      public var __typename: String {
+        get {
+          return resultMap["__typename"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      public var id: String {
+        get {
+          return resultMap["id"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "id")
+        }
+      }
+
+      public var name: String? {
+        get {
+          return resultMap["name"] as? String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "name")
+        }
+      }
+    }
+  }
+}
