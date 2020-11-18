94bac8568a08ea9d914f17295e633a4fb70dc15c
AtsukiTak <takatomgoo@gmail.com>
2020-11-18 13:12:48 +0900

    cargo make schema.json タスクの追加

---
+++ server/mina-app/examples/print_graphql_introspection.rs
@@ -0,0 +1,101 @@
+use async_graphql::Request;
+use mina_app::graphql::GraphQL;
+
+#[tokio::main]
+async fn main() {
+    let req = Request::new(QUERY);
+    let res = GraphQL::schema().execute(req).await;
+    let schema_json = res.data.into_json().unwrap();
+    print!("{:#}", schema_json);
+}
+
+const QUERY: &str = r#"
+query {
+  __schema {
+    queryType { name }
+    mutationType { name }
+    subscriptionType { name }
+    types {
+      ...FullType
+    }
+    directives {
+      name
+      description
+      locations
+      args {
+        ...InputValue
+      }
+    }
+  }
+}
+fragment FullType on __Type {
+  kind
+  name
+  description
+  fields(includeDeprecated: true) {
+    name
+    description
+    args {
+      ...InputValue
+    }
+    type {
+      ...TypeRef
+    }
+    isDeprecated
+    deprecationReason
+  }
+  inputFields {
+    ...InputValue
+  }
+  interfaces {
+    ...TypeRef
+  }
+  enumValues(includeDeprecated: true) {
+    name
+    description
+    isDeprecated
+    deprecationReason
+  }
+  possibleTypes {
+    ...TypeRef
+  }
+}
+fragment InputValue on __InputValue {
+  name
+  description
+  type { ...TypeRef }
+  defaultValue
+}
+fragment TypeRef on __Type {
+  kind
+  name
+  ofType {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
+          name
+          ofType {
+            kind
+            name
+            ofType {
+              kind
+              name
+              ofType {
+                kind
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"#;
